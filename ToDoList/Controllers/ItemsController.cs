9be02ba6d6fc69fb5d738330b064d8146c95f225
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-12-23 18:42:35 -0800

    add details page

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -18,5 +18,11 @@ namespace ToDoList.Controllers
             List<Item> model = db.Items.ToList();
             return View(model);
         }
+
+        public IActionResult Details(int id)
+        {
+            Item thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
     }
 }
